441476c6c4ae0bf43bbe7d24b492430be66c4153
Eduardo Valenzuela <evalenzuela@catarxis.com>
2016-10-20 14:11:45 -0500

    Initial Commit

---
+++ CarsCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarsCollector
+//
+//  Created by Eduardo Valenzuela on 10/20/16.
+//  Copyright © 2016 Catarxis Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
